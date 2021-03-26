e01b8aba62676a4a0355fb7dead2e95bcae251e5
Luiz Otavio <luizomf@gmail.com>
2021-03-26 11:31:54 -0300

    Reducer

---
+++ src/contexts/CounterContext/reducer.js
@@ -0,0 +1,23 @@
+import { initialState } from '.';
+import * as actionTypes from './action-types';
+
+export const reducer = (state, action) => {
+  switch (actionTypes.type) {
+    case actionTypes.INCREASE:
+      return { ...state, counter: state.counter + 1 };
+    case actionTypes.DECREASE:
+      return { ...state, counter: state.counter - 1 };
+    case actionTypes.RESET:
+      return { ...initialState };
+    case actionTypes.SET_COUNTER:
+      return { ...state, ...action.payload };
+    case actionTypes.ASYNC_INCREASE_START:
+      return { ...state, loading: true };
+    case actionTypes.ASYNC_INCREASE_END:
+      return { ...state, loading: false, counter: state.counter + 1 };
+    case actionTypes.ASYNC_INCREASE_ERROR:
+      return { ...state, loading: false };
+  }
+
+  return state;
+};
